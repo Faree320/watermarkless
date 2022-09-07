093261ee43605bc7ee51e26283d09a4f9c0b417b
Faree320 <100018647+Faree320@users.noreply.github.com>
2022-09-07 15:10:37 +0500

    first commit

--- offline_tiktok/settings.py
+++ offline_tiktok/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yzi_$&j*y8l0kqgfla_ik1%um*^l)g^jhjn!9r$97blke03+&2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
